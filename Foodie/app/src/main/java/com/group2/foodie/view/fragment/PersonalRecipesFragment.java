482c5523c8aa6c08bad2115805f71bf8952441ed
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2022-03-24 13:02:56 +0100

    Personal recipes

---
+++ Foodie/app/src/main/java/com/group2/foodie/view/fragment/PersonalRecipesFragment.java
@@ -0,0 +1,71 @@
+package com.group2.foodie.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.group2.foodie.R;
+import com.group2.foodie.list.RecipeAdapter;
+import com.group2.foodie.model.Recipe;
+import com.group2.foodie.viewmodel.PersonalRecipesViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonalRecipesFragment extends Fragment {
+
+    private RecyclerView recipesRecycler;
+    private RecipeAdapter recipeAdapter;
+    private PersonalRecipesViewModel viewModel;
+    private NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_personal_recipes, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recipesRecycler = view.findViewById(R.id.personalRecipes_recycleView);
+        recipesRecycler.hasFixedSize();
+        recipesRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        ArrayList<Recipe> recipeList = new ArrayList<>();
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+        recipeList.add(new Recipe("Burger", R.id.personal_recipe_item_picture, new ArrayList<>(), "1. Kill the pig",true, "Food"));
+
+        recipeAdapter = new RecipeAdapter(recipeList);
+        //recipeAdapter = new RecipeAdapter(viewModel.getPersonalRecipes());
+        recipesRecycler.setAdapter(recipeAdapter);
+
+        // TODO
+        //navController = Navigation.findNavController(getActivity(), R.id.main_activity_fragment);
+
+        recipeAdapter.setOnClickListener(v -> {
+           //  TODO
+            // navController.navigate(R.id.);
+        });
+    }
+}
---
+++ Foodie/app/src/main/java/com/group2/foodie/viewmodel/PersonalRecipesViewModel.java
@@ -0,0 +1,21 @@
+package com.group2.foodie.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.group2.foodie.model.Recipe;
+
+import java.util.List;
+
+public class PersonalRecipesViewModel extends ViewModel {
+
+/*    private RecipeRepository repository;
+
+    public MainActivityViewModel(){
+        repository = Repository.getInstance();
+    }
+
+    public LiveData<List<Recipe>> getPersonalRecipes() {
+        return repository.getPersonalRecipes();
+    }*/
+}
