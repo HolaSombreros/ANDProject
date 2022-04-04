115ad5a3cf5ed6a177c8360b4fe7401f70945824
realfoxsterdota <foxsterdota@gmail.com>
2022-04-04 13:01:11 +0200

    Added login functionality.

--- Foodie/app/src/main/java/com/group2/foodie/viewmodel/LoginViewModel.java
+++ Foodie/app/src/main/java/com/group2/foodie/viewmodel/LoginViewModel.java
@@ -1,6 +1,31 @@
 package com.group2.foodie.viewmodel;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class LoginViewModel extends ViewModel {
+    private MutableLiveData<String> errorMessage;
+
+    public LoginViewModel() {
+        errorMessage = new MutableLiveData<>();
+    }
+
+    public LiveData<String> getErrorMessage() {
+        return errorMessage;
+    }
+
+    public boolean login(String email, String password) {
+        if (email == null || email.isEmpty()) {
+            errorMessage.setValue("Please enter an email address");
+            return false;
+        }
+
+        if (password == null || password.isEmpty()) {
+            errorMessage.setValue("Please enter a password");
+            return false;
+        }
+
+        return true;
+    }
 }
